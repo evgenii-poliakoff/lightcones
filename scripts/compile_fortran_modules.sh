a677726d1176919ca7afe20270fa7557a92e0557
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-01-08 19:11:43 +0300

    fix script

--- scripts/compile_fortran_modules.sh
+++ scripts/compile_fortran_modules.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 echo "Current directory: $(pwd)"
-cd ../src
+cd ./src
 python3 -m numpy.f2py --quiet -c fastmul.f90 -m fastmul --fcompiler=intelem --f90flags=-fast
 python3 -m numpy.f2py --quiet -c evolution_chained2.f90 -m evolution_chained2  --fcompiler=intelem --f90flags=-fast
 python3 -m numpy.f2py --quiet -c evolution_chained2_kicked.f90 -m evolution_chained2_kicked  --fcompiler=intelem --f90flags=-fast
