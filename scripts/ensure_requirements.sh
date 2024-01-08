222d81c468c3de5f7b53ca136fd30d00cf7be716
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-01-08 23:16:49 +0300

    fix

--- scripts/ensure_requirements.sh
+++ scripts/ensure_requirements.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-CURRENT_DEVENV_VERSION="0.0.0"
-export CURRENT_DEVENV_VERSION="0.0.0"
+./scripts/version.sh
+export CURRENT_DEVENV_VERSION=$DEVENV_VERSION
 
 CI_PATH_PREFIX="$HOME/ci"
 export CI_PATH_PREFIX
@@ -10,12 +10,16 @@ FILE_PATH="$CI_PATH_PREFIX/version.sh"
 
 if [ ! -f "$FILE_PATH" ]
 then
+    echo "Version not found: Installing requirements."
    ./scripts/install_requirements.sh
 else
     $CI_PATH_PREFIX/version.sh
     if ["$CURRENT_DEVENV_VERSION" != "$DEVENV_VERSION"] 
     then
+        echo "Versions do not match: Installing requirements."
         ./scripts/install_requirements.sh
+    else
+        echo "Version is up to date: Do not installing requirements."
     fi
 fi
 
--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -15,4 +15,7 @@ then
     SOURCE="./scripts/version.sh"
     DESTINATION="$CI_PATH_PREFIX/"
     cp $SOURCE $DESTINATION
+    echo "Succesfully udpated requisites to version $CURRENT_DEVENV_VERSION."
+else
+    echo "There were errors while installing requisites: not updating version."
 fi
