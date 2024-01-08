b84b9c9fe632cb89c243795c839ed31877b3bd2a
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-01-08 18:25:50 +0300

    Initial commit

---
+++ scripts/compile_fortran_modules.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+cd ../src/
+python3 -m numpy.f2py --quiet -c fastmul.f90 -m fastmul --fcompiler=intelem --f90flags=-fast
+python3 -m numpy.f2py --quiet -c evolution_chained2.f90 -m evolution_chained2  --fcompiler=intelem --f90flags=-fast
+python3 -m numpy.f2py --quiet -c evolution_chained2_kicked.f90 -m evolution_chained2_kicked  --fcompiler=intelem --f90flags=-fast
+python3 -m numpy.f2py --quiet -c evolution.f90 -m evolution  --fcompiler=intelem --f90flags=-fast
+python3 -m numpy.f2py --quiet -c evolution2.f90 -m evolution2  --fcompiler=intelem --f90flags=-fast
+python3 -m numpy.f2py --quiet -c local_op.f90 -m local_op --fcompiler=intelem --f90flags=-fast
