1c063ed1bd3011e30154b8440778eff99033e0f6
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-01-10 03:18:10 +0300

    fix anaconda installer

--- scripts/install_anaconda.sh
+++ scripts/install_anaconda.sh
@@ -5,7 +5,7 @@ then
     echo "Anaconda3 not found. Installing ..."
     wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
     bash ./Anaconda3-2023.09-0-Linux-x86_64.sh -b -p $HOME/anaconda3
-    eval "$($CI_PATH_PREFIX/anaconda3/bin/conda shell.bash hook)"
+    eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
     conda init
 else
     echo "Anaconda3 is already installed"
