3ff7154f58af976b2c08a6b3ad450608311abf7e
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-02-07 20:52:44 +0300

    download anaconda to dependencies

--- scripts/install_anaconda.sh
+++ scripts/install_anaconda.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
+FILEPATH="./dependencies/Anaconda3-2023.09-0-Linux-x86_64.sh"
+RAW_URL="https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh"
+
 if ! command -v conda list &> /dev/null
 then
     echo "Anaconda3 not found. Installing ..."
-    wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
-    bash ./Anaconda3-2023.09-0-Linux-x86_64.sh -b -p $HOME/anaconda3
+    curl --create-dirs -o $FILEPATH -L $RAW_URL
+    bash ./dependencies/Anaconda3-2023.09-0-Linux-x86_64.sh -b -p $HOME/anaconda3
     eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
     conda init
 else
