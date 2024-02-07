e75d4bb4eff395d3916690a66a6ec75e43a6807c
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-02-07 21:00:07 +0300

    download ifort to dependencies

--- scripts/install_intel_oneapi.sh
+++ scripts/install_intel_oneapi.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+FILEPATH1="./dependencies/l_BaseKit_p_2023.0.0.25537_offline.sh"
+RAW_URL1="https://registrationcenter-download.intel.com/akdlm/irc_nas/19079/l_BaseKit_p_2023.0.0.25537_offline.sh"
+FILEPATH2="./dependencies/l_HPCKit_p_2023.0.0.25400_offline.sh"
+RAW_URL2="https://registrationcenter-download.intel.com/akdlm/irc_nas/19084/l_HPCKit_p_2023.0.0.25400_offline.sh"
+
+
 if ! command -v ifort -v &> /dev/null
 then
     echo "Intel Fortran not found. Installing ..."
-    wget https://registrationcenter-download.intel.com/akdlm/irc_nas/19079/l_BaseKit_p_2023.0.0.25537_offline.sh
-    sh ./l_BaseKit_p_2023.0.0.25537_offline.sh -l install1.log -a --silent --eula accept 
-    wget https://registrationcenter-download.intel.com/akdlm/irc_nas/19084/l_HPCKit_p_2023.0.0.25400_offline.sh
-    sh ./l_HPCKit_p_2023.0.0.25400_offline.sh -l install2.log -a --silent --eula accept 
+    curl --create-dirs -o $FILEPATH1 -L $RAW_URL1
+    sh ./dependencies/l_BaseKit_p_2023.0.0.25537_offline.sh -l install1.log -a --silent --eula accept 
+    curl --create-dirs -o $FILEPATH2 -L $RAW_URL2
+    sh ./dependencies/l_HPCKit_p_2023.0.0.25400_offline.sh -l install2.log -a --silent --eula accept 
 else
     echo "Intel Fortran is already installed."
 fi
