949e0c95793e5bc1210dfc233ed5c092d8971c32
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-03-06 02:46:48 +0300

    lightcones.spread

---
+++ lightcones/__init__.py
@@ -0,0 +1,33 @@
+import numpy as np
+import lightcones.linalg as la
+from lightcones.solvers.schrodinger import solve
+
+# input:
+#    e    array of onsite energies e[0] ... e[n_sites - 1]
+#    h    array of hoppings h[0] ... h[n_sites - 2]
+#    nt   number of time steps during which to compute spread
+#    dt   size of one time step
+# output:
+#    alpha = spread(e, h, nt, dt)
+#         alpha[k, l] = \alpha(l*dt)_{k} where alpha is from the spread equation  
+def spread(e, h, nt, dt):
+    n_sites = len(e)
+    # first-quantized Hamiltonian
+    H = la.tridiag(e, h)
+
+    # initial condition: quantum on site 0 of the chain
+    phi_0 = np.zeros(n_sites, dtype = complex)
+    phi_0[0] = 1 # initially quantum is on the site 0
+
+    # Here we store the propagated orbitals (the spread)
+    phi_lc = np.zeros((n_sites, nt), dtype = np.cdouble) 
+
+    def apply_h(ti, phi_in, phi_out):
+        la.mv(H, phi_in, phi_out, cout=1)
+
+    def eval_o(ti, phi):
+        phi_lc[:, ti] = phi
+    
+    solve(0, nt-1, dt, apply_h, phi_0, eval_o = eval_o)
+    
+    return phi_lc
