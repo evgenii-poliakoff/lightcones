779e899b1cd12082473f77bf710dd55f03513daa
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-03-08 12:21:30 +0300

    user decides whether to install anaconda

--- scripts/install_anaconda.sh
+++ scripts/install_anaconda.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
 
+# Check if INSTALL_ANACONDA is set and if it's 'yes'
+if [ -n "$INSTALL_ANACONDA" ] && [ "$INSTALL_ANACONDA" == "yes" ]; then
+    # Continue execution
+    echo "INSTALL_ANACONDA is set to intelem, will check for Anaconda3 python dependency..."
+else
+    # Exit with a message if FCOMPILER is not 'intelem'
+    echo "INSTALL_ANACONDA is not set to yes, will not check for Anaconda3 python dependency. Exiting"
+    exit 0
+fi
+
 FILEPATH="./dependencies/Anaconda3-2023.09-0-Linux-x86_64.sh"
 RAW_URL="https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh"
 
--- scripts/read_setup_cfg.sh
+++ scripts/read_setup_cfg.sh
@@ -2,6 +2,9 @@
 
 # Define the file and section
 CONFIG_FILE="setup.cfg"
+
+# read build_ext/fcompiler
+
 SECTION="build_ext"
 KEY="fcompiler"
 
@@ -12,3 +15,13 @@ export FCOMPILER="$fcompiler"
 
 # Print the value to confirm it was successfully read
 echo "Detected fcompiler value: $FCOMPILER"
+
+# read user_options/install_anaconda
+
+SECTION="user_options"
+KEY="install_anaconda"
+
+# Use sed to extract the value of fcompiler
+install_anaconda=$(sed -n "/^\[${SECTION}\]/,/^\[/ {/^${KEY}=/p}" $CONFIG_FILE | awk -F'=' '{print $2}')
+
+export INSTALL_ANACONDA="$install_anaconda"
