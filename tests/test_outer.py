6f21d820247584db80b4155c584d4fef6e72384f
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-03-10 11:53:13 +0300

    more tests for outer

--- tests/test_outer.py
+++ tests/test_outer.py
@@ -115,4 +115,29 @@ def test_outer_fermion_2():
     assert np.allclose(rho, rho_expected, rtol=1e-5, atol=1e-8), \
         f"rho not match the ethalon"
     
+def test_outer_fermion_3():
+    max_quanta = 5
+    space = fock.space(statistics = 'Fermi', num_modes = 4, max_total_occupation = max_quanta)
+    psi = np.zeros(space.dimension, dtype = complex)
+    a = np.sin(pi/5)
+    b = np.cos(pi/5)
+    occupations = [0, 1, 1, 1]
+    psi[space.index(occupations)] = np.sqrt(1.0/2) * a
+    occupations = [0, 1, 1, 0]
+    psi[space.index(occupations)] = np.sqrt(1.0/2) * b
+    occupations = [1, 1, 1, 1]
+    psi[space.index(occupations)] = np.sqrt(1.0/2) * b
+    occupations = [1, 1, 1, 0]
+    psi[space.index(occupations)] = np.sqrt(1.0/2) * (-a)
+    pr = [[space.outer(0, 0, 0), space.outer(0, 1, 0)],
+          [space.outer(1, 0, 0), space.outer(1, 1, 0)]]
+    rho = np.zeros((2, 2), dtype = complex)
+    for p in range(2):
+        for q in range(2):
+            rho[p, q] = np.vdot(psi, pr[p][q] @ psi)
+    rho_expected = np.zeros((2, 2), dtype = complex)
+    rho_expected[0, 0] = 1.0/2
+    rho_expected[1, 1] = 1.0/2
+    assert np.allclose(rho, rho_expected, rtol=1e-5, atol=1e-8), \
+        f"rho not match the ethalon" 
     
