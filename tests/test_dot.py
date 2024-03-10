e9cc615e340c379072a6140b31e50a941bea30f4
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-03-10 16:15:24 +0300

    test mul and dot

--- tests/test_dot.py
+++ tests/test_dot.py
@@ -5,7 +5,8 @@ def test_dot():
     a = np.array([1.0 + 1j, 2.0], dtype = complex)
     b = np.array([3.0, 1.0 - 1j * 2], dtype = complex)
     c = dot(a, b)
-    assert np.isclose(c, (1.0 - 1j) * 3.0 + 2.0 * (1.0 - 1j *2))
+    assert np.isclose(c, (1.0 - 1j) * 3.0 + 2.0 * (1.0 - 1j *2), rtol=1e-5, atol=1e-8), \
+        f"c not match the ethalon"
     
 def test_dot_2():
     a = np.array([1.0 + 1j, 2.0], dtype = complex)
@@ -16,5 +17,5 @@ def test_dot_2():
                   [dot(a, b[1][0]), dot(a, b[1][1])]]
     for i in range(2):
         for j in range(2):
-            np.isclose(c[i][j], c_expected[i][j], rtol=1e-5, atol=1e-8), \
+            assert np.isclose(c[i][j], c_expected[i][j], rtol=1e-5, atol=1e-8), \
                 f"c not match the ethalon"
---
+++ tests/test_mul.py
@@ -0,0 +1,27 @@
+import numpy as np
+from lightcones.linalg import mul
+
+def test_mul():
+    max_quanta = 5
+    local_bounds = [1, 1, max_quanta, max_quanta]
+    space = fock.space(statistics = 'Bose', num_modes = 4, max_local_occupations = local_bounds)
+    a = space.outer(0, 0, 0)
+    b = space.vacuum_state()
+    c = mul(a, b)
+    assert np.close(c, b, rtol=1e-5, atol=1e-8), \
+        f"c not match the ethalon"
+    
+def test_mul_2():
+    max_quanta = 5
+    local_bounds = [1, 1, max_quanta, max_quanta]
+    space = fock.space(statistics = 'Bose', num_modes = 4, max_local_occupations = local_bounds)
+    a = [[space.outer(0, 0, 0), space.outer(0, 1, 0)],
+           [space.outer(1, 0, 0), space.outer(1, 1, 0)]]
+    b = space.vacuum_state()
+    c = mul(a, b)
+    c_expected = [[mul(a[0][0], b), mul(a[0][1], b)],
+                  [mul(a[1][0], b), mul(a[1][1], b)]]
+    for i in range(2):
+        for j in range(2):
+            np.allclose(c[i][j], c_expected[i][j], rtol=1e-5, atol=1e-8), \
+                f"c not match the ethalon"
