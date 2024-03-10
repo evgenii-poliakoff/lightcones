65e64ecb6d6c476ecc4de5d805f4b5624dd34ac1
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-03-10 11:35:06 +0300

    test outer - fermions

--- tests/test_outer.py
+++ tests/test_outer.py
@@ -22,3 +22,23 @@ def test_outer_boson():
     assert np.allclose(rho, rho_expected, rtol=1e-5, atol=1e-8), \
         f"rho not match the ethalon"
     
+def test_outer_fermion():
+    max_quanta = 5
+    space = fock.space(statistics = 'Fermi', num_modes = 4, max_total_occupations = max_quanta)
+    psi = np.zeros(space.dimension, dtype = complex)
+    occupations = [0, 1, 1, 1]
+    psi[space.index(occupations)] = np.sqrt(0.5)
+    occupations = [1, 1, 1, 0]
+    psi[space.index(occupations)] = np.sqrt(0.5)
+    pr = [[space.outer(0, 0, 0), space.outer(0, 1, 0)],
+          [space.outer(1, 0, 0), space.outer(1, 1, 0)]]
+    rho = np.zeros((2, 2), dtype = complex)
+    for p in range(2):
+        for q in range(2):
+            rho[p, q] = np.vdot(psi, pr[p][q] @ psi)
+    rho_expected = np.zeros((2, 2), dtype = complex)
+    rho_expected[0, 0] = 0.5
+    rho_expected[1, 1] = 0.5
+    assert np.allclose(rho, rho_expected, rtol=1e-5, atol=1e-8), \
+        f"rho not match the ethalon"
+    
