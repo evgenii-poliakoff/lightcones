6ad4d89a234f998bda148017e8aabd1d02039bb6
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-03-17 00:27:45 +0300

    add fermion_chain

--- lightcones/models/__init__.py
+++ lightcones/models/__init__.py
@@ -3,10 +3,12 @@ __all__ = [
     'to_star',
     'spin_boson', 
     'fermion_2_fermion_leads',
-    'spin_chain'
+    'spin_chain',
+    'fermion_chain'
 ]
 
 from .star2chain import *
 from .spin_boson import *
 from .fermion_2_fermion_leads import *
 from .spin_chain import *
+from .fermion_chain import *
---
+++ lightcones/models/fermion_chain.py
@@ -0,0 +1,23 @@
+from lightcones import fock
+
+class fermion_chain:
+    def __init__(self, length, max_total_excitations):
+    
+        local_bounds = [1]*length
+        space = fock.space(statistics = 'Fermi', num_modes = length, \
+            max_total_occupation = max_total_excitations)
+        self.space = space
+        
+        self.dimension = space.dimension
+        self.zero_op = space.emptyH
+        self.eye = space.eye
+        
+        self.length = length
+        self.max_total_excitations = max_total_excitations
+        
+        a = space.annihilate
+        a_dag = space.create
+        self.a = a
+        self.a_dag = a_dag
+            
+        self.outer = space.outer
