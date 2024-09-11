d2fc5a1f12c1664f4e6f15b98e8f00721fcc6065
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-09-11 14:22:04 +0300

    star2chain.py

--- lightcones/models/star2chain.py
+++ lightcones/models/star2chain.py
@@ -1,6 +1,9 @@
 import numpy as np
 from lightcones import linalg as la
 
+# given star form of the environment
+# i.e. grid of frequencies w and spectral density values J(W)
+# compute the equivalent chain represention of the environment
 def to_chain(w, J, n = None):
     if n is None:
         n = len(w)
@@ -11,9 +14,15 @@ def to_chain(w, J, n = None):
     h = h[1:]
     return coupling, e, h
 
+# given the chain form of the environment
+# i.e. grid of on-site energies e
+# and grid of hoppings h
+# return the star form of the environment
+# i.e. grid of frequencies w and grid of spectral densities J(w)
 def to_star(coupling, e, h):
     H = la.tridiag(e, h)
     H_dense = H.todense()
     w, modes = la.find_eigs_ascending(H_dense)
     J = modes[0,:]**2 * coupling**2
     return w, J 
+
