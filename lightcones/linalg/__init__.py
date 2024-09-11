29dabde90d5307027d5728a2accbab380fc92ae4
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-09-11 15:33:30 +0300

    +

--- lightcones/linalg/__init__.py
+++ lightcones/linalg/__init__.py
@@ -21,9 +21,14 @@ from typing import Any
 from ._fastmul import fastmul
 from . import _dlancz
 
+# compute
+# vout = cin * m @ vin + cout * vout
+# using the fortran optimized code
+# (on intel compiler it is faster then numpy)
 def mv(m, vin, vout, cin=1, cout=0):
     fastmul(m.data, m.indices, m.indptr, cin, vin, cout, vout)
 
+# lanczos algorithm
 def lancz(w, J, n = None):
     if n is None:
         n = len(w)
@@ -32,6 +37,9 @@ def lancz(w, J, n = None):
         raise RuntimeError("dlancz ierr not zero: " + str(ierr))   
     return a, b
 
+# make tridiagonal matrix 
+# e: diagonal
+# h: upper and lower diagonal
 def tridiag(e, h):
     data = [np.concatenate((h, np.array([0]))), np.array(e), np.concatenate((np.array([0]), h))]
     diags = np.array([-1, 0, 1])
@@ -39,16 +47,22 @@ def tridiag(e, h):
     n = len(e)
     return spdiags(data, diags, n, n).tocsc()
 
+# make dyad |ket><bra|
 def dyad(ket, bra):
     return np.kron(ket, bra.T.conj())
 
+# make column vector |ket>
+# from the array ket
 def as_column_vector(ket):
     return ket[:, None]
 
+# take Hermitean part of the square matrix m
 def make_hermitean(m):
     m += m.T.conj()
     m /= 2
 
+# find k largest eigenvalues of matrix m
+# sorted in the descending order
 def find_largest_eigs(m, k = None):
     n = len(m)
     if k is None:
@@ -59,6 +73,8 @@ def find_largest_eigs(m, k = None):
     v = np.flip(v, axis = 1)
     return (e, v)
 
+# find k smallest eigenvalues of matrix m
+# sorted in the ascending order
 def find_smallest_eigs(m, k = None):
     n = len(m)
     if k is None:
@@ -67,10 +83,14 @@ def find_smallest_eigs(m, k = None):
     e, v = eigh(m, subset_by_index = [0, k - 1])
     return (e, v)
 
+# find all eigenvalues of matrix m
+# sorted in the ascending order
 def find_eigs_ascending(m):
     e, v = eigh(m)
     return (e, v)
 
+# find all eigenvalues of matrix m
+# sorted in the descending order
 def find_eigs_descending(m):
     e, v = eigh(m)
     e = np.flip(e)
@@ -80,9 +100,11 @@ def find_eigs_descending(m):
 def is_list_list_of_any(a):
     return all(isinstance(sublist, list) for sublist in a)
 
+# check whether a is a csc_matrix type
 def is_sparse_matrix(a):
     return isinstance(a, scipy.sparse.csc_matrix)
 
+# check whether a is a vector type
 def is_vector(a):
     return isinstance(a, np.ndarray) and len(a.shape) == 1
 
@@ -106,6 +128,10 @@ def kron_list_list(a, b):
                     kr[i1 * n2 + i2].append(kron(a[i1][j1], b[i2][j2]))
     
     return kr
+
+# kron of sparse matrices a and b
+# if either a or b are lists of sparse matrices
+# then do elementwise kron
 
 def kron(a, b):
     if is_sparse_matrix(a) and is_sparse_matrix(b):
@@ -154,6 +180,10 @@ def mul_listlist_listlist(a, b):
     
     return ml
 
+# compute a @ b
+# if a and b are matrices then matrix multiplication
+# if a is matrix and b is vector then matrix-vector multiplication
+# if either a or b are lists then elementwise multiplication
 def mul(a, b):
     if is_sparse_matrix(a) and is_vector(b):
         return mul_sparse_vector(a, b)
@@ -182,6 +212,10 @@ def dot_vector_list_list(a, b):
             row.append(dot(a, b[i][j]))
         o.append(row)
     return o
+
+# inner product of vectors
+# if either a or b are lists of vectors
+# then elementwise dot product
 
 def dot(a, b):
     if is_vector(a) and is_vector(b):
