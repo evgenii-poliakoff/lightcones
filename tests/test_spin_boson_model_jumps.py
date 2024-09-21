cec46445e7f8a21cc17bbbaaa4cb4d8f9ee07f82
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-09-22 02:21:12 +0300

    fix

--- tests/test_spin_boson_model_jumps.py
+++ tests/test_spin_boson_model_jumps.py
@@ -6,7 +6,7 @@ from .cases import spin_boson_model_jumps
 
 def test_s_z_av():
     tr = spin_boson_model_jumps.trajectory()
-    s_z_av_table = tr.solve()
+    s_z_av_table = tr.compute_s_z_av()
     with pathlib.Path("./tests/cases/s_z_av_spin_boson.txt").open() as f:
         s_z_av_expected =  np.loadtxt(f)
     assert np.allclose(s_z_av_table, s_z_av_expected, rtol=1e-5, atol=5e-4), \
