9d361f747757b71d3704961307e482da952a7265
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-12-04 18:39:10 +0300

    +tests

--- tests/test_spinfull_fermions.py
+++ tests/test_spinfull_fermions.py
@@ -47,3 +47,22 @@ def test_spinfull_fermions():
     assert np.allclose(V[:, 2], psi_expected, atol=tol), \
         f"psi does not match"
     
+    # case 3
+    
+    f = spinfull_fermions(2)
+    
+    t = 0.2
+    U = 0.3
+    H_hopp = sum([sum([- t * (f.a_dag[s][i] @ f.a[s][i - 1] + f.a_dag[s][i - 1] @ f.a[s][i]) for s in range(2)]) for i in range(2)])
+    H_int = sum([U * f.n[0][i] @ f.n[1][i] for i in range(2)])
+    H = H_hopp + H_int
+    
+    E, V = ll.find_eigs_ascending(H.todense())
+    E_expected = np.array([-6.63941029805e-01, -4.00000000000e-01, -4.00000000000e-01,
+       -1.00000000000e-01, -1.00000000000e-01,  0.00000000000e+00,
+        0.00000000000e+00,  0.00000000000e+00,  9.99200722163e-16,
+        3.00000000000e-01,  4.00000000000e-01,  4.00000000000e-01,
+        6.00000000000e-01,  7.00000000000e-01,  7.00000000000e-01,
+        9.63941029805e-01])
+    assert np.allclose(E, E_expected, atol=tol), \
+        f"E does not match"
