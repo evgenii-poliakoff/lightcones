433e92f843648394a3f1a93d58e1852e55f03c52
Evgenii Poliakoff <e.polyakov@rqc.ru>
2024-12-04 17:40:43 +0300

    + tests

--- tests/test_spinfull_fermions.py
+++ tests/test_spinfull_fermions.py
@@ -22,18 +22,28 @@ def test_spinfull_fermions():
     assert np.allclose(E, E_expected, atol=tol), \
         f"E does not match"
         
+    psi_expected = np.array([0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j])
+    assert np.allclose(V[:, 3], psi_expected, atol=tol), \
+        f"psi does not match"
+        
     # case 2
     
     f = spinfull_fermions(1)
     
     U = 1
-    mu = U / 2
+    mu = U * (1/2.0 + 1/3.0)
     
     H_S = U * f.n[0][0] @ f.n[1][0] - mu * (f.n[0][0] + f.n[1][0])
     
     E, V = ll.find_eigs_ascending(H_S.todense())
     
-    E_expected = np.array([-0.5, -0.5,  0. ,  0.])
+    E_expected = np.array([-0.83333333333, -0.83333333333, -0.66666666667,  0.           ])
     assert np.allclose(E, E_expected, atol=tol), \
         f"E does not match"
+    psi_expected = np.array([1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j])
+    assert np.allclose(V[:, 3], psi_expected, atol=tol), \
+        f"psi does not match"
+    psi_expected = np.array([0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j])
+    assert np.allclose(V[:, 2], psi_expected, atol=tol), \
+        f"psi does not match"
     
