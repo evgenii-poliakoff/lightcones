261e6c60e06495646650d3fdb355aa790d59a037
Evgenii Poliakoff <e.polyakov@rqc.ru>
2025-03-11 18:53:32 +0300

    only test_moving_frame temporarily

--- tests/test_moving_frame.py
+++ tests/test_moving_frame.py
@@ -12,7 +12,7 @@ def complex_converter(x):
     x = x.decode('utf-8')
     return complex(x.strip('()'))
 
-@pytest.mark.skip(reason="This test fails on ar")
+#@pytest.mark.skip(reason="This test fails on ar")
 def test_moving_frame():
     # chain
     n_sites = 100
