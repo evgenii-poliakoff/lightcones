2711e87891965f9a1aa70c29b7dfed913231bee2
Evgenii Poliakoff <e.polyakov@rqc.ru>
2025-03-12 03:28:29 +0300

    test cd frame

--- tests/test_moving_frame.py
+++ tests/test_moving_frame.py
@@ -4,9 +4,9 @@ import numpy as np
 import pathlib
 import pytest
 
-import numpy as np
+import math
 import lightcones as lc
-import lightcones.linalg as la
+import lightcones.linalg as ll
 
 def complex_converter(x):
     return complex(x.strip('()'))
@@ -49,27 +49,67 @@ def test_moving_frame():
         
     with pathlib.Path("./tests/cases/spread_min.txt").open() as f:
         spread_min_expected = np.loadtxt(f, dtype=complex, converters=complex_converter)
-    assert np.allclose(spread_min[: n_rel, :], spread_min_expected[: n_rel, :], rtol=1e-5, atol=2e-8), \
-        f"spread_min does not match the expected"
         
-    with pathlib.Path("./tests/cases/U_min.txt").open() as f:
-        U_min_expected =  np.loadtxt(f, dtype=complex, converters=complex_converter)
-    assert np.allclose(U_min, U_min_expected, rtol=1e-5, atol=1e-8), \
-        f"U_min does not match the expected"
+    # compare the commutators based on spread
         
+    comm_expected = []
+    comm_actual = []
+        
+    for ti in range(0, nt, 10):
+        m_in = lc.m_in(ti_arrival, ti)
+        for tj in range(ti, nt, 10):
+            comm_expected.append(np.vdot( spread_min_expected[: m_in, ti],  spread_min_expected[: m_in, tj]))
+            comm_actual.append(np.vdot( spread_min[: m_in, ti],  spread_min[: m_in, tj]))
+        
+    assert np.allclose(np.array(comm_expected), np.array(comm_actual), rtol=1e-5, atol=2e-8), \
+        f"comm"
+        
+    # test U_min
+    
+    spread_actual = U_min.T.conj() @ spread_min
+    assert np.allclose(spread_actual, spread, rtol=1e-5, atol=1e-8), \
+        f"U_min"
+    
+    # test rho_plus_min
+    
+    rho_plus_actual = U_min.T.conj() @ rho_plus_min @ U_min
+    assert np.allclose(rho_plus_actual, rho_plus, rtol=1e-5, atol=1e-8), \
+        f"rho_plus_min"
+        
+    # test eigenvalues of rho_plus_min
+    
     with pathlib.Path("./tests/cases/rho_plus_min.txt").open() as f:
         rho_plus_min_expected =  np.loadtxt(f, dtype=complex, converters=complex_converter)
-    assert np.allclose(rho_plus_min, rho_plus_min_expected, rtol=1e-5, atol=1e-8), \
-        f"rho_plus_min does not match the expected"
+    
+    e, _ =  ll.find_largest_eigs(rho_plus_min, k = n_rel)
+    e_expected, _ =  ll.find_largest_eigs(rho_plus_min_expected, k = n_rel)
+    
+    assert np.allclose(e, e_expected, rtol=1e-5, atol=1e-8), \
+        f"e"
         
     # causal diamond frame
+    
     cd_dim = 4
     spread_cd, U_cd = lc.causal_diamond_frame(spread_min, ti_arrival, U_min, rho_plus_min, dt, rtol, cd_dim)
     
-    with pathlib.Path("./tests/cases/spread_cd.txt").open() as f:
-        spread_cd_expected =  np.loadtxt(f, dtype=complex, converters=complex_converter)
-    assert np.allclose(spread_cd, spread_cd_expected, rtol=1e-5, atol=1e-8), \
-        f"spread_min does not match the expected"
+    # test commutator
+    
+    comm_cd_actual = []
+    for ti in range(0, nt, 10):
+        m_out, m_in = lc.get_inout_range(ti_arrival, ti, cd_dim)
+        for tj in range(ti, nt, 10):
+            m_out_, m_in_ = lc.get_inout_range(ti_arrival, tj, cd_dim)
+            vec_1 = spread_cd[: m_in, ti]
+            vec_2 = spread_cd[: m_in_, tj]
+            if (m_in_ > m_in):
+                vec_1 = np.concatenate((vec_1, np.zeros(m_in_ - m_in)))
+            for i in range(m_in, m_in_):
+                if not U_cd[i] is None:
+                    vec_1[i - cd_dim : i] = U_cd[i] @ vec_1[i - cd_dim : i]            
+            comm_cd_actual.append(np.vdot(vec_1, vec_2))
+
+    assert np.allclose(np.array(comm_expected), np.array(comm_cd_actual), rtol=1e-5, atol=2e-8), \
+        f"comm_cd"
         
     marker = np.empty((0, 0))
     U_cd_computed = [m if m is not None else marker for m in U_cd]
