e6aba9757095134062842ccaba396538da451fc8
evgenii-poliakoff <e.a.polyakov@gmail.com>
2025-06-11 13:49:46 +0000

    Fix readme

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 conda_env_dir="${script_dir}"/../conda-env
 
+"${script_dir}"/ensure_conda_env.sh
+
 eval "$($HOME/miniforge3/bin/conda shell.bash hook)"
 
 echo INFO Building lightcones package
